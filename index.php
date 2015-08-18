f2934fc183dde013feefda8d57778faff68fc0ac
root <root@ip-10-170-38-58.ec2.internal>
2015-08-18 22:51:21 +0000

    Adding endpoint for saving data files.

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Silex\Application;
 use LIUM\Utils\Environment;
 
@@ -11,6 +13,14 @@ $environment->set("input_directory_path", "data/input")
 
 $app = new Application();
 $app['sshs_environment'] = $environment;
+
+$app->before(function (Request $request) {
+  if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+    $data = json_decode($request->getContent(), true);
+    $request->request->replace(is_array($data) ? $data : array());
+  }
+});
+
 $app->get('/assets', function() use($app) { 
     $output = array();
     $input_dir = $app['sshs_environment']->get('input_directory_path');
@@ -33,6 +43,27 @@ $app->get('/assets', function() use($app) {
       }
     }
     return $app->json($output);
+});
+
+$app->post('/segments', function (Request $request) use ($app) {
+  $output_dir = $app['sshs_environment']->get('output_directory_path');
+  if (!empty($request->get('segments')) && !empty($request->get('asset'))) {
+    $info = pathinfo($request->get('asset'));
+    $file_name = $info['filename'] . '-output-' . date('Y-m-d-his') . '.json';
+    $url = $output_dir . '/' . $file_name;
+    try {
+      $fp = fopen($url, 'w');
+      $outcome = fwrite($fp, json_encode($request->get('segments')));
+      fclose($fp);
+      return new Response('Created new output file: ' . $url, 201);
+    }
+    catch (Exception $e) {
+      return new Response($e->getMessage(), 400);
+    }
+  }
+  else {
+    return new Response('Malformed request.', 400);
+  }
 });
 
 $app->run();
